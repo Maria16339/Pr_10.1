8cb5aff6e4c80626c617aeef0ee72628db39797c
Mariia Danylevych <danilevich2003.2@gmail.com>
2020-12-10 13:28:27 +0200

    Pr_10.1

---
+++ Pr_10.1.C++/Pr_10.1.C++/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int Count();
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Кількість  'abc' в файлі t.txt: " << Count() << endl;
+    return 0;
+}
+int Count()
+{
+    ifstream t("t.txt");
+    string s;
+
+    int k = 0;
+
+    while (getline(t, s))
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'a' &&
+                s[i + 1] == 'b' &&
+                s[i + 2] == 'c')
+            {
+                k++;
+            }
+        }
+    }
+    return k;
+}
